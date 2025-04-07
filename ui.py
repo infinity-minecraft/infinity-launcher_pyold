96eff155cae134f7db7b18062eb4e4a0595c8045
leinadR <baton105@icloud.com>
2025-04-07 19:39:02 +0300

    fix java install

--- ui.py
+++ ui.py
@@ -64,7 +64,7 @@ class InstallThread(QThread):
             urllib.request.urlretrieve("https://download.oracle.com/java/17/archive/jdk-17.0.12_windows-x64_bin.msi", f"{os.path.expanduser('~')}/.infl/jdk-17.0.12_windows-x64_bin.msi")
             self.log_signal.emit("Установка Java...")
             msi_path = os.path.expanduser('~') + '/.infl/jdk-17.0.12_windows-x64_bin.msi'
-            subprocess.run(["msiexec", "/i", msi_path, "/quiet", "/norestart"])
+            subprocess.run(["msiexec", "/i", msi_path, "/norestart"])
             self.log_signal.emit("Java установлена")
 
         from install_minecraft import m_install
